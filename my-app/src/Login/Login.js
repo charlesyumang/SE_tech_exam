dfb256689c9b562761c271af186ab90978de9393
charlesyumang <yumangcharles095@gmail.com>
2025-05-07 17:42:07 +0800

    added login page

--- my-app/src/Login/Login.js
+++ my-app/src/Login/Login.js
@@ -1,7 +1,52 @@
 import React from "react";
+import "./Login.css";
+import {
+  LOGIN_CONSTANT,
+  PASSWORD_CONSTANT,
+  USERNAME_CONSTANT,
+  SUBMIT_CONSTANT,
+} from "../constants";
+import { Input } from "antd";
 
 function Login() {
-  return <h1>asd</h1>;
+  return (
+    <>
+      <div className="wrapper d-flex align-items-center justify-content-center w-100">
+        <div className="login-form">
+          <div className="login-text">
+            <h2>{LOGIN_CONSTANT}</h2>
+          </div>
+          <form className="needs-validation">
+            <div className="form-group mb-2">
+              <label htmlFor="username" className="form-label">
+                {USERNAME_CONSTANT}
+              </label>
+              <Input
+                type="text"
+                placeholder={USERNAME_CONSTANT}
+                required
+                onKeyDown={(e) => e.key === " " && e.preventDefault()}
+              />
+            </div>
+
+            <div className="form-group mb-2">
+              <label htmlFor="password" className="form-label">
+                {PASSWORD_CONSTANT}
+              </label>
+              <Input.Password
+                type="password"
+                placeholder={PASSWORD_CONSTANT}
+                required
+              />
+            </div>
+            <button className="submit btn btn-dark mx-auto d-block w-75">
+              {SUBMIT_CONSTANT}
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
---
+++ my-app/src/constants.js
@@ -0,0 +1,5 @@
+export const LOGIN_CONSTANT = "Login";
+export const USERNAME_CONSTANT = "Username";
+export const PASSWORD_CONSTANT = "Password";
+export const REQUIRED_PASSWORD_CONSTANT = "Please enter password";
+export const SUBMIT_CONSTANT = "Submit";
